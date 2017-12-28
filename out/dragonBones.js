9b086e93ad952480277ce5899f2be2879227e9bb
quyetdx <quyetdx@sandinh.net>
2017-12-28 15:22:21 +0700

    Get size from frame first before looking up in region

--- out/dragonBones.js
+++ out/dragonBones.js
@@ -13611,8 +13611,13 @@ var dragonBones;
                 if (this.armature.armatureData.defaultSkin && this.armature.armatureData.defaultSkin.displays) {
                     var properties = Object.getOwnPropertyNames(this.armature.armatureData.defaultSkin.displays);
                     var Data = this.armature.armatureData.defaultSkin.displays[properties[0]][0];
-                    if (Data && Data.texture && Data.texture.region) {
-                        width = Data.texture.region.width;
+                    if (Data && Data.texture) {
+                        if (Data.texture.frame) {
+                            width = Data.texture.frame.width;
+                        }
+                        else if (Data.texture.region) {
+                            width = Data.texture.region.width;
+                        }
                     }
                 }
                 return width;
@@ -13626,8 +13631,13 @@ var dragonBones;
                 if (this.armature.armatureData.defaultSkin && this.armature.armatureData.defaultSkin.displays) {
                     var properties = Object.getOwnPropertyNames(this.armature.armatureData.defaultSkin.displays);
                     var Data = this.armature.armatureData.defaultSkin.displays[properties[0]][0];
-                    if (Data && Data.texture && Data.texture.region) {
-                        height = Data.texture.region.height;
+                    if (Data && Data.texture) {
+                        if (Data.texture.frame) {
+                            height = Data.texture.frame.height;
+                        }
+                        else if (Data.texture.region) {
+                            height = Data.texture.region.height;
+                        }
                     }
                 }
                 return height;
--- src/dragonBones/pixi/PixiArmatureDisplay.ts
+++ src/dragonBones/pixi/PixiArmatureDisplay.ts
@@ -229,8 +229,12 @@ namespace dragonBones {
             if (this.armature.armatureData.defaultSkin && this.armature.armatureData.defaultSkin.displays) {
                 const properties = Object.getOwnPropertyNames(this.armature.armatureData.defaultSkin.displays)
                 const Data = <ImageDisplayData>this.armature.armatureData.defaultSkin.displays[properties[0]][0]
-                if (Data && Data.texture && Data.texture.region) {   
-                    width = Data.texture.region.width
+                if (Data && Data.texture) {
+                    if (Data.texture.frame) {   
+                        width = Data.texture.frame.width
+                    } else if (Data.texture.region) {
+                        width = Data.texture.region.width
+                    }
                 }
             }
             return width
@@ -240,8 +244,12 @@ namespace dragonBones {
             if (this.armature.armatureData.defaultSkin && this.armature.armatureData.defaultSkin.displays) {
                 const properties = Object.getOwnPropertyNames(this.armature.armatureData.defaultSkin.displays)
                 const Data = <ImageDisplayData>this.armature.armatureData.defaultSkin.displays[properties[0]][0]
-                if (Data && Data.texture && Data.texture.region) {
-                    height = Data.texture.region.height
+                if (Data && Data.texture) {
+                    if (Data.texture.frame) {
+                        height = Data.texture.frame.height
+                    } else if (Data.texture.region) {
+                        height = Data.texture.region.height
+                    }
                 }
             }
             return height
